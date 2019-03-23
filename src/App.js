4d95b07133b3d14900603b73174bdf915172a888
fredzeng <979738295@qq.com>
2019-03-24 06:22:28 +0800

    fix: fix bugs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/home'
-import LoginPage from './components/loginpage'
-import Login from './components/login'
+import Home from './components/home';
+import LoginPage from './components/loginpage';
+import Login from './components/login';
 import './App.css';
 
-// import Router from './router/index.js'
-
-
-class App extends React.Component{
-  render(){
-    return(
-   <Router >
-        <div style={{height:'100%'}}>
-        <Switch>
-          <Route exact path="/" component={LoginPage}/>
-          <Route path="/home" component={Home}/>
-          <Route path="/login" component={Login}/>
-        </Switch>
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div style={{ height: '100%' }}>
+          <Switch>
+            <Route exact path="/" component={LoginPage} />
+            <Route path="/home" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
         </div>
-    </Router>
-    )
+      </Router>
+    );
   }
 }
-
-
 
 export default App;
